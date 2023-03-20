b09ca2c6ef3ef8744f04ba18462b40071d857e2f
Rhylen Nguyen <rhylennguyen@gmail.com>
2023-03-20 14:04:47 -0400

    Initial commit

---
+++ src/Board.svelte
@@ -0,0 +1,48 @@
+<script>
+  import Square from "./Square.svelte";
+  import { icons } from "./characters.js";
+
+  let size = 5;
+  let squares = [];
+
+  console.log(icons);
+
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+  }
+  
+  const range = icons.length;
+  let random;
+
+  function getNewCharacter() {
+    random = getRandomInt(0, range - 1);
+    squares.forEach((square) => {
+      if (icons[random] == square) {
+        getNewCharacter();
+      }
+    });
+    return icons[random];
+  }
+
+  for (let i = 0; i < size * size; i++) {
+    let character = getNewCharacter();
+    console.log(character);
+    squares.push(character);
+  }
+</script>
+
+<div class="board" style="--board_size: {size}">
+  {#each squares as square, i}
+    <Square value={square} />
+  {/each}
+</div>
+
+<style>
+  .board {
+    display: grid;
+    grid-template-columns: repeat(var(--board_size), auto);
+    width: 500px;
+  }
+</style>
---
+++ src/Square.svelte
@@ -0,0 +1,29 @@
+<script>
+    export let value;
+  </script>
+  
+  <style>
+    .square {
+      width: 90px;
+      height: 90px;
+      background-color: whitesmoke;
+      border: 2px solid black;
+      margin: 5px;
+      padding: 5px;
+      font-size: 20px;
+      text-align: center;
+    }
+
+    .square img {
+        width:100%;
+        height: 100%;
+    }
+  
+    .square:hover {
+      border: 2px solid red;
+    }
+  </style>
+  
+  <button class="square"><img src="{value.url}" alt="{value.name}"/></button>
+  
+  
